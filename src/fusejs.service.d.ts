73829aa91b2d2d8be4a50848129cdc48e48daea5
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2021-04-26 12:24:19 +0200

    Prod build in progress

---
+++ src/fusejs.service.d.ts
@@ -0,0 +1,15 @@
+import { FuseOptions } from 'fuse.js';
+export interface AngularFusejsOptions<T> extends FuseOptions<T> {
+    supportHighlight?: boolean;
+    fusejsHighlightKey?: string;
+    fusejsScoreKey?: string;
+    minSearchTermLength?: number;
+    maximumScore?: number;
+    highlightTag?: string;
+}
+export declare class FusejsService<T> {
+    private defaultOptions;
+    searchList(list: Array<T>, searchTerms: string, options?: AngularFusejsOptions<T>): any[];
+    private deepClone;
+    private handleHighlight;
+}
